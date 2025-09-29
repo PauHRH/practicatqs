408000c3aca7a048fd88031d31220ec4f8c99a74
PauHRH <pau2005lm@gmail.com>
2025-09-29 11:46:17 +0200

    first commit

---
+++ src/test/test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class testTest {
+
+    @Test
+    void sampleTest() {
+
+
+
+        assertTrue(true);
+    }
+}
