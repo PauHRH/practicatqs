d5cdc5d387d99c9bcd006ef8771d60c880786519
NIU1707058 <1707058@uab.cat>
2025-10-13 12:19:40 +0200

    Declaració clases Codi i Configuració

---
+++ src/main/java/es/uab/tqs/mastermind/Codi.java
@@ -0,0 +1,15 @@
+package es.uab.tqs.mastermind;
+
+import java.util.List;
+
+public class Codi {
+    private final List<Integer> digits;
+
+    public Codi(List<Integer> digits) {
+        this.digits = digits;
+    }
+
+    public List<Integer> getDigits() {
+        
+    }
+}
---
+++ src/main/java/es/uab/tqs/mastermind/Configuracio.java
@@ -0,0 +1,25 @@
+package es.uab.tqs.mastermind;
+
+public class Configuracio {
+    private int longitud;
+    private int numIntents;
+    private int rangDigits;
+
+    public Configuracio(int longitud, int numIntents, int rangDigits) {
+        this.longitud = longitud;
+        this. numIntents = numIntents;
+        this.rangDigits = rangDigits;
+    }
+
+    public int getLongitud() {
+
+    }
+
+    public int getNumIntents() {
+
+    }
+
+    public int getRangDigits() {
+        
+    }
+}
