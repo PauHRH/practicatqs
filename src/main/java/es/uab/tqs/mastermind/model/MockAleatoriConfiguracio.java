99df58817124427021c0136dda0fd1ed526907ac
PauHRH <pau2005lm@gmail.com>
2025-11-09 19:21:03 +0100

    Declaracio Test VerificaIntent, MockAleatoriConfiguracio, VerificaIntent

---
+++ src/main/java/es/uab/tqs/mastermind/model/MockAleatoriConfiguracio.java
@@ -0,0 +1,20 @@
+package es.uab.tqs.mastermind.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MockAleatoriConfiguracio implements Aleatori{
+    List<List<Integer>> listaIntentsRetornar = new ArrayList<>();
+	
+	public MockAleatoriConfiguracio()
+	{
+		
+		
+	}
+	
+	public List<Integer> getAltNumber(int longitud)
+	{
+        return null;
+	}
+}
---
+++ src/test/java/es/uab/tqs/mastermind/TestVerificaIntent.java
@@ -0,0 +1,33 @@
+package es.uab.tqs.mastermind;
+import org.junit.jupiter.api.Test;
+
+import es.uab.tqs.mastermind.model.Aleatori;
+import es.uab.tqs.mastermind.model.CodiSecret;
+import es.uab.tqs.mastermind.model.VerificaIntent;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class TestVerificaIntent {
+    CodiSecret codi;
+	Aleatori aleatori;
+	
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@Test
+	void testVerificaIntent() {
+	
+	}
+
+	
+	
+	void testGetArrayPosicions() {
+		fail("Not yet implemented");
+	}
+}
