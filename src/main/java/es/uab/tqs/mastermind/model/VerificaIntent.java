c5dda6be41d5639f27469fd1c7bc67b371e2cc9c
PauHRH <pau2005lm@gmail.com>
2025-11-09 19:17:20 +0100

    Implementació clase base VerificaIntent

---
+++ src/main/java/es/uab/tqs/mastermind/model/VerificaIntent.java
@@ -0,0 +1,21 @@
+package es.uab.tqs.mastermind.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class VerificaIntent {
+    CodiSecret codi;
+	List<Integer> codiIntroduit;
+	
+	public VerificaIntent(CodiSecret codi)
+	{
+	
+	}
+	
+	public List<Integer> getArrayPosicions(List<Integer> codiIntroduit) // 0 = incorrecte, 1 = correcte, 2 = num correcte pos no.
+	{
+	   return null;
+		
+	}
+}
