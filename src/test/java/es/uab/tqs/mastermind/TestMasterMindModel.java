f7521246a4bbd8bc7bbd1a3fee70a3d2a9964a77
NIU1707058 <1707058@uab.cat>
2025-11-10 11:24:28 +0100

    Declaracio test del model

---
+++ src/test/java/es/uab/tqs/mastermind/TestMasterMindModel.java
@@ -0,0 +1,32 @@
+package es.uab.tqs.mastermind;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import es.uab.tqs.mastermind.model.Aleatori;
+import es.uab.tqs.mastermind.model.MasterMindModel;
+import es.uab.tqs.mastermind.model.MockAleatori;
+import es.uab.tqs.mastermind.model.MockAleatoriConfiguracio;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestMasterMindModel {
+
+
+	@Test
+	void testNovaPartida() {
+		
+	}
+	
+	@Test
+	void testFerJugada() {
+		
+	}
+
+	@Test
+	void testGetArrayCorrectes() {
+		
+	}
+
+}
