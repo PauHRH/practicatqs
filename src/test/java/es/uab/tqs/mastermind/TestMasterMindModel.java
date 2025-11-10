7051335b10fd7c649079dea9ff88075d4c2f4d77
NIU1707058 <1707058@uab.cat>
2025-11-10 11:54:11 +0100

    Implementacio test getArrayCorrectes

--- src/test/java/es/uab/tqs/mastermind/TestMasterMindModel.java
+++ src/test/java/es/uab/tqs/mastermind/TestMasterMindModel.java
@@ -26,6 +26,62 @@ class TestMasterMindModel {
 
 	@Test
 	void testGetArrayCorrectes() {
+		//valor correcte
+		//generem primer codi aleatori 1,3,2,3
+		
+		MasterMindModel m1 = new MasterMindModel(4,5,new MockAleatoriConfiguracio());
+
+		//correcte amb totes pos iguals
+		assertEquals(Arrays.asList(1,1,1,1), m1.getArrayCorrectes(Arrays.asList(1,3,2,3)));
+		//primera pos incorrecta
+		assertEquals(Arrays.asList(0,1,1,1), m1.getArrayCorrectes(Arrays.asList(4,3,2,3)));
+		//segona pos incorrecta
+		assertEquals(Arrays.asList(1,0,1,1), m1.getArrayCorrectes(Arrays.asList(1,5,2,3)));
+		assertEquals(Arrays.asList(1,1,0,1), m1.getArrayCorrectes(Arrays.asList(1,3,6,3)));
+		assertEquals(Arrays.asList(1,1,1,0), m1.getArrayCorrectes(Arrays.asList(1,3,2,8)));
+		
+		//dos pos incorrectes:
+		assertEquals(Arrays.asList(0,0,1,1), m1.getArrayCorrectes(Arrays.asList(6,5,2,3)));
+		assertEquals(Arrays.asList(1,0,0,1), m1.getArrayCorrectes(Arrays.asList(1,5,7,3)));
+		assertEquals(Arrays.asList(1,1,0,0), m1.getArrayCorrectes(Arrays.asList(1,3,8,8)));
+		assertEquals(Arrays.asList(0,1,0,1), m1.getArrayCorrectes(Arrays.asList(4,3,6,3)));
+		assertEquals(Arrays.asList(1,0,1,0), m1.getArrayCorrectes(Arrays.asList(1,9,2,4)));
+	
+		
+		//tres pos incorrectes
+		assertEquals(Arrays.asList(0,0,0,1), m1.getArrayCorrectes(Arrays.asList(7,6,5,3)));
+		assertEquals(Arrays.asList(1,0,0,0), m1.getArrayCorrectes(Arrays.asList(1,5,7,8)));
+		assertEquals(Arrays.asList(0,1,0,0), m1.getArrayCorrectes(Arrays.asList(4,3,5,8)));
+		assertEquals(Arrays.asList(0,0,1,0), m1.getArrayCorrectes(Arrays.asList(5,6,2,4)));	
+		
+		//tot incorrecte
+		//tres pos incorrectes
+		assertEquals(Arrays.asList(0,0,0,0), m1.getArrayCorrectes(Arrays.asList(7,6,5,4)));
+	
+		//una pos correcta dos no una casi
+		assertEquals(Arrays.asList(0,2,1,0), m1.getArrayCorrectes(Arrays.asList(4,1,2,9)));
+		
+		//dos pos correctes
+		assertEquals(Arrays.asList(2,2,1,0), m1.getArrayCorrectes(Arrays.asList(3,1,2,9)));
+		
+		//valor fuera de rango
+		try
+		{
+		m1.getArrayCorrectes(Arrays.asList(-1,1,2,4));
+		assertTrue(false);
+		}catch(Error e) {}
+		try
+		{
+		m1.getArrayCorrectes(Arrays.asList(-1,1,10,4));
+		assertTrue(false);
+		} catch (Error e) {}
+		
+		//lista amb valors menors a codi
+		try
+		{
+		m1.getArrayCorrectes(Arrays.asList(1,1,10));
+		assertTrue(false);
+		} catch (AssertionError e) {}
 		
 	}
 
