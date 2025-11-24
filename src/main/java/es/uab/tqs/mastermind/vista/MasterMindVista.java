3aa0ddb74c2cbcb6a505fc8d9c97e31b54d74fa3
PauHRH <pau2005lm@gmail.com>
2025-11-24 12:07:13 +0100

    Declaracio abstracta Vista

---
+++ src/main/java/es/uab/tqs/mastermind/vista/MasterMindVista.java
@@ -0,0 +1,26 @@
+package es.uab.tqs.mastermind.vista;
+import java.util.Scanner;
+import java.util.List;
+
+public abstract class MasterMindVista {
+    protected Scanner scanner;
+
+    public MasterMindView()
+    {
+
+    }
+
+    public void mostrarBenvinguda()
+    {
+
+    }
+
+    public void demanarMaxIntents()
+    {
+
+    }
+
+    public abstract int recullLongitud();
+    public abstract int recullMaxIntents();
+    
+}
