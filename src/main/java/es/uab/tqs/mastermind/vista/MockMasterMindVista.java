eb6f8601bc87badbc08d47cb03a906ec927abdf1
PauHRH <pau2005lm@gmail.com>
2025-11-30 17:30:38 +0100

    Actualització controlador, avançat

--- src/main/java/es/uab/tqs/mastermind/vista/MockMasterMindVista.java
+++ src/main/java/es/uab/tqs/mastermind/vista/MockMasterMindVista.java
@@ -1,12 +1,59 @@
 package es.uab.tqs.mastermind.vista;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MockMasterMindVista extends MasterMindVista {
+
+    // Inputs per test
+    private int longitudSimulada = 4;
+    private int maxIntentsSimulats = 5;
+    private List<List<Integer>> intentsUsuari = new ArrayList<>();
+    private int indexIntent = 0;
+
+    public boolean haMostratGuanyar = false;
+    public boolean haMostratPerdre = false;
+    public List<List<Integer>> resultatsMostrats = new ArrayList<>();
 
     public MockMasterMindVista()
     {
         super();
     }
-    
+
+    // metodes per tests
+    public void setConfiguracioInicial(int longitud, int maxIntents) {
+        this.longitudSimulada = longitud;
+        this.maxIntentsSimulats = maxIntents;
+    }
+
+    public void afegirIntentUsuari(List<Integer> intent) {
+        this.intentsUsuari.add(intent);
+    }
+
+    @Override
+    public List<Integer> recullIntent(int longitudEsperada) {
+        if (indexIntent < intentsUsuari.size()) {
+            return intentsUsuari.get(indexIntent++);
+        }
+        return new ArrayList<>(); 
+    }
+
+    @Override
+    public void mostrarResultat(List<Integer> resultat) {
+        resultatsMostrats.add(resultat);
+    }
+
+    @Override
+    public void mostrarGuanyar() {
+        haMostratGuanyar = true;
+    }
+
+    @Override
+    public void mostrarPerdre(List<Integer> codiSecret) {
+        haMostratPerdre = true;
+    }
+
+
     @Override
     public int recullLongitud()
     {
@@ -18,4 +65,6 @@ public class MockMasterMindVista extends MasterMindVista {
     {
         return 5;
     }
+
+
 }
--- src/test/java/es/uab/tqs/mastermind/TestMasterMindControlador.java
+++ src/test/java/es/uab/tqs/mastermind/TestMasterMindControlador.java
@@ -7,6 +7,7 @@ import es.uab.tqs.mastermind.model.CodiSecret;
 import es.uab.tqs.mastermind.model.MasterMindModel;
 import es.uab.tqs.mastermind.model.MockAleatori;
 import es.uab.tqs.mastermind.model.MockAleatoriConfiguracio;
+import es.uab.tqs.mastermind.vista.MockMasterMindVista;
 import es.uab.tqs.mastermind.controlador.MasterMindControlador;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -14,29 +15,42 @@ import org.junit.jupiter.api.Test;
 
 public class TestMasterMindControlador {
     
-    MasterMindModel model;
-    MasterMindVista vista;
+MasterMindModel model;
+    MockMasterMindVista vistaMock; // Vista mock
     MasterMindControlador controlador;
+    MockAleatori aleatoriMock;
 
     @BeforeEach
 	void setUp() {
-        vista = new MasterMindMockVista();
+        aleatoriMock = new MockAleatori(); // Codi fixe: 1,2,2,1
+        model = new MasterMindModel(aleatoriMock);
+        vistaMock = new MockMasterMindVista();
+        controlador = new MasterMindControlador(model, vistaMock);
 
 	}
 
     @Test
     void testIniciarPartida()
     {
+        vistaMock.setConfiguracioInicial(4, 1);
+        vistaMock.afegirIntentUsuari(Arrays.asList(1, 2, 2, 1));
+
+        controlador.iniciarPartida();
+
+        assertTrue(vistaMock.haMostratGuanyar, "Debe ganar");
+        assertFalse(vistaMock.haMostratPerdre);
+        assertEquals(1, model.getIntentsFets());
+
+        /*
         Aleatori aleatori = new MockAleatori();
-        model = new MasterMindModel(aleatori);
-);
+        model = new MasterMindModel(aleatori););
         controlador = new MasterMindControlador(vista, model);
         controlador.iniciarPartida();
 
         //comprovem que agafa bé valors de la vista
         assertEquals(model.getIntentsMax(), 5);
         assertEquals(model.getCodi().getLen(), 4);
-
+        */
 
 
     }
