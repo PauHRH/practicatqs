7813ff8ecc7f98a447d034a41ce0999178120b8a
PauHRH <pau2005lm@gmail.com>
2025-11-30 17:07:02 +0100

    Declaració controlador i tests controlador

--- src/main/java/es/uab/tqs/mastermind/controlador/MasterMindControlador.java
+++ src/main/java/es/uab/tqs/mastermind/controlador/MasterMindControlador.java
@@ -1,5 +1,20 @@
 package es.uab.tqs.mastermind.controlador;
 
+import es.uab.tqs.mastermind.model.MasterMindModel;
+import es.uab.tqs.mastermind.vista.MasterMindVista;
+
 public class MasterMindControlador {
+
+    private MasterMindModel model;
+    private MasterMindVista view;
     
+    public MasterMindControlador(MasterMindModel model, MasterMindVista view)
+    {
+    	this.model = model;
+    	this.view = view;
+    }
+
+    public void iniciarPartida() {
+
+    }
 }
---
+++ src/test/java/es/uab/tqs/mastermind/testControlador.java
@@ -0,0 +1,17 @@
+package es.uab.tqs.mastermind;
+import org.junit.jupiter.api.Test;
+
+import es.uab.tqs.mastermind.model.Aleatori;
+import es.uab.tqs.mastermind.model.CodiSecret;
+import es.uab.tqs.mastermind.model.MockAleatori;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class testControlador {
+    
+}
