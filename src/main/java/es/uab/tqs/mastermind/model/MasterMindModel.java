794bb28c8d22c68417ae7e655243bf25f8f58915
NIU1707058 <1707058@uab.cat>
2025-12-02 18:31:54 +0100

    Correcció errors MasterMindModel

--- src/main/java/es/uab/tqs/mastermind/model/MasterMindModel.java
+++ src/main/java/es/uab/tqs/mastermind/model/MasterMindModel.java
@@ -1,6 +1,5 @@
 package es.uab.tqs.mastermind.model;
 import java.util.List;
-import java.util.ArrayList;
 
 public class MasterMindModel {
     private CodiSecret codi;
@@ -11,7 +10,17 @@ public class MasterMindModel {
 	
 	public List<Integer> getArrayCorrectes(List<Integer> codiProposat)
 	{
-        assert(codiProposat.size() == codi.getLen()) : "Error, longitud codi proposat incorrecte";
+		for (int c : codiProposat)
+		{
+			if (c < 0 || c > 9)
+				throw new IllegalArgumentException("Numero incorrecte en el codi proposat.");
+		}
+		
+		if (codiProposat.size() != codi.getLen()) 
+		{
+			throw new IllegalArgumentException("Longitud codi proposat incorrecta");
+		}
+		
 		VerificaIntent vi = new VerificaIntent(codi);
 		return vi.getArrayPosicions(codiProposat);
 	}
@@ -69,23 +78,34 @@ public class MasterMindModel {
 	
 	public void novaPartida(int longCodi, int intentsMax)
 	{
-		//precondició:
-		assert((longCodi >= 2 && longCodi <= 6)) : "Longitud codi incorrecta";
-		assert((intentsMax >= 1 && intentsMax <= 10)) : "Num intents incorrecta";
-		
+		if (longCodi < 2 || longCodi > 6) 
+		{
+			throw new IllegalArgumentException("Longitud codi incorrecta");
+		}
+
+		if (intentsMax < 1 || intentsMax > 10)
+		{
+			throw new IllegalArgumentException("Num intents incorrecta");
+		}
+
 		this.codi = new CodiSecret(this.aleatori, longCodi);
 		this.codi.generarCodi();
 		this.intentsMax = intentsMax;
 		this.intentsFets = 0;
 		this.haGuanyat = false;
-
-        //postcondició: 
-        assert(intentsFets == 0) : "IntentsFets no inicialitzat a 0";
 	}
 	
 	public boolean ferJugada(List<Integer> codiProposat)
 	{
-		assert(codiProposat.size() == codi.getLen() || intentsFets <= intentsMax ) : "Error, fallida";
+		if (codiProposat.size() != codi.getLen()) 
+		{
+			throw new IllegalArgumentException("Longitud codi proposat incorrecta");
+		}
+
+		if (intentsFets >= intentsMax)
+		{
+			throw new IllegalArgumentException("Intents màxims fets");
+		}
 
 		intentsFets++;
 		
