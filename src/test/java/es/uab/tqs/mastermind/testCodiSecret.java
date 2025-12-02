d456651f441d99c164f28f618d59b984b2b75329
NIU1707058 <1707058@uab.cat>
2025-12-02 18:30:34 +0100

    Millorar amb caixa negra i blanca testCodiSecret

--- src/test/java/es/uab/tqs/mastermind/testCodiSecret.java
+++ src/test/java/es/uab/tqs/mastermind/testCodiSecret.java
@@ -21,6 +21,7 @@ class TestCodiSecret {
 	{
 		alt = new MockAleatori();
 		codi = new CodiSecret(alt, 4);
+		codi.generarCodi(); // Genera [1, 2, 2, 1]
 	}
 	
 
@@ -34,28 +35,191 @@ class TestCodiSecret {
 
     @Test
 	void testComprovaCodi() {
-		// valor normal
-		codi.setCodi(Arrays.asList(1,3,4,2));
-		assertTrue(codi.comprovaCodi(Arrays.asList(1,3,4,2)));
+		/*
+			Particions equivalents per longitud 4 (Valor entremig)
+		*/
+		// Llista igual: codiIntroduit es exactament igual a codi
+		assertTrue(codi.comprovaCodi(Arrays.asList(1,2,2,1)));
+		// Llista diferent: codiIntroduit es diferent a codi
+		assertFalse(codi.comprovaCodi(Arrays.asList(1,5,2,1)));
 		assertFalse(codi.comprovaCodi(Arrays.asList(9,1,3,5)));
-		
-		//valor de 2 valors:
-		codi.setCodi(Arrays.asList(5,4));
-		assertTrue(codi.comprovaCodi(Arrays.asList(5,4)));
-		assertFalse(codi.comprovaCodi(Arrays.asList(2,6)));
-		
-		//valor de 1 valor
-		codi.setCodi(Arrays.asList(5));
-		assertTrue(codi.comprovaCodi(Arrays.asList(5)));
-		assertFalse(codi.comprovaCodi(Arrays.asList(2)));
-		
-		//introdueix llista buida, falla
-		codi.setCodi(Arrays.asList(5));
-		try
-		{
-			codi.comprovaCodi(Arrays.asList());
+		// Llista buida
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList()));
 			assertTrue(false);
-		} catch(Exception exc) {
-		}
+		} catch (Exception e) {}
+		// Llista null
+		try {
+			assertFalse(codi.comprovaCodi(null));
+			assertTrue(false);
+		} catch (Exception e) {}
+
+		/*
+			Particions equivalents per longitud 1 (Valor límit)
+		*/
+		codi.setCodi(Arrays.asList(1));
+		// Llista igual: codiIntroduit es exactament igual a codi
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList(1)));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista diferent: codiIntroduit es diferent a codi
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList(2)));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista buida
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList()));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista null
+		try {
+			assertFalse(codi.comprovaCodi(null));
+			assertTrue(false);
+		} catch (Exception e) {}
+
+		/*
+			Particions equivalents per longitud 2 (Valor frontera)
+		*/
+		codi.setCodi(Arrays.asList(5, 4));
+		// Llista igual: codiIntroduit es exactament igual a codi
+		assertTrue(codi.comprovaCodi(Arrays.asList(5, 4)));
+		// Llista diferent: codiIntroduit es diferent a codi
+		assertFalse(codi.comprovaCodi(Arrays.asList(2, 6)));
+		// Llista buida
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList()));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista null
+		try {
+			assertFalse(codi.comprovaCodi(null));
+			assertTrue(false);
+		} catch (Exception e) {}
+
+		/*
+			Particions equivalents per longitud 3 (Valor límit)
+		*/
+		codi.setCodi(Arrays.asList(6, 7, 2));
+		// Llista igual: codiIntroduit es exactament igual a codi
+		assertTrue(codi.comprovaCodi(Arrays.asList(6, 7, 2)));
+		// Llista diferent: codiIntroduit es diferent a codi
+		assertFalse(codi.comprovaCodi(Arrays.asList(2, 6, 7)));
+		// Llista buida
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList()));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista null
+		try {
+			assertFalse(codi.comprovaCodi(null));
+			assertTrue(false);
+		} catch (Exception e) {}
+
+		/*
+			Particions equivalents per longitud 5 (Valor límit)
+		*/
+		codi.setCodi(Arrays.asList(1, 8, 3, 5, 7));
+		// Llista igual: codiIntroduit es exactament igual a codi
+		assertTrue(codi.comprovaCodi(Arrays.asList(1, 8, 3, 5, 7)));
+		// Llista diferent: codiIntroduit es diferent a codi
+		assertFalse(codi.comprovaCodi(Arrays.asList(2, 6, 7, 5, 3)));
+		// Llista buida
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList()));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista null
+		try {
+			assertFalse(codi.comprovaCodi(null));
+			assertTrue(false);
+		} catch (Exception e) {}
+
+		/*
+			Particions equivalents per longitud 6 (Valor frontera)
+		*/
+		codi.setCodi(Arrays.asList(1, 8, 3, 5, 7, 2));
+		// Llista igual: codiIntroduit es exactament igual a codi
+		assertTrue(codi.comprovaCodi(Arrays.asList(1, 8, 3, 5, 7, 2)));
+		// Llista diferent: codiIntroduit es diferent a codi
+		assertFalse(codi.comprovaCodi(Arrays.asList(2, 6, 7, 5, 3, 4)));
+		// Llista buida
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList()));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista null
+		try {
+			assertFalse(codi.comprovaCodi(null));
+			assertTrue(false);
+		} catch (Exception e) {}
+
+		/*
+			Particions equivalents per longitud 7 (Valor límit)
+		*/
+		codi.setCodi(Arrays.asList(1, 8, 3, 5, 7, 2, 1));
+		// Llista igual: codiIntroduit es exactament igual a codi
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList(1, 8, 3, 5, 7, 2, 1)));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista diferent: codiIntroduit es diferent a codi
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList(2, 6, 7, 5, 3, 4, 3)));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista buida
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList()));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista null
+		try {
+			assertFalse(codi.comprovaCodi(null));
+			assertTrue(false);
+		} catch (Exception e) {}
+
+		/*
+			Particions equivalents per longitud 8 (Valor fora de rang per part dels positius)
+		*/
+		codi.setCodi(Arrays.asList(1, 8, 3, 5, 7, 2, 1, 9));
+		// Llista igual: codiIntroduit es exactament igual a codi
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList(1, 8, 3, 5, 7, 2, 1, 9)));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista diferent: codiIntroduit es diferent a codi
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList(2, 6, 7, 5, 3, 4, 3, 9)));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista buida
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList()));
+			assertTrue(false);
+		} catch (Exception e) {}
+		// Llista null
+		try {
+			assertFalse(codi.comprovaCodi(null));
+			assertTrue(false);
+		} catch (Exception e) {}
+
+		/*
+			Decision i Condition coverage
+		*/
+		// comprovacio true: codi.size() != codiIntroduit.size()
+		codi.setCodi(Arrays.asList(1, 2, 2, 1));
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList(1, 2, 3)));
+			assertTrue(false);
+		} catch (Exception e) {}
+
+		// comprovacio true: codiIntroduit.size() > 6
+		codi.setCodi(Arrays.asList(1, 2, 2, 1));
+		try {
+			assertFalse(codi.comprovaCodi(Arrays.asList(1, 2, 3, 4, 5, 6, 7)));
+			assertTrue(false);
+		} catch (Exception e) {}
 	}
 }
