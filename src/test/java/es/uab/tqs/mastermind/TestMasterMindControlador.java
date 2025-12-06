03409c40cd6ad875ecd9ca7f9d74571d2018f9fc
PauHRH <pau2005lm@gmail.com>
2025-12-06 12:46:32 +0100

    Loop testing aniuat funcio esIntentRepetit

--- src/test/java/es/uab/tqs/mastermind/TestMasterMindControlador.java
+++ src/test/java/es/uab/tqs/mastermind/TestMasterMindControlador.java
@@ -296,7 +296,71 @@ MasterMindModel model;
         controlador.esIntentRepetit(intent7);
 
 
+        /*
+            Loop testing
 
+            Bucle intern:
+         */
+        List<Integer> intentBase = Arrays.asList(1, 2, 3, 4);
+        List<List<Integer>> historialUnic = new ArrayList<>();
+        historialUnic.add(intentBase);
+        
+        controlador = new MasterMindControlador(model, vistaMock);
+        inserirHistorial(historialUnic);
+
+        // Cas 1: 1 pasada pel loop intern
+        assertFalse(controlador.esIntentRepetit(Arrays.asList(5, 2, 3, 4)));
+
+        // Cas 2: 2 pasades pel loop intern
+        assertFalse(controlador.esIntentRepetit(Arrays.asList(1, 5, 3, 4)));
+
+        // Cas 3: m pasades pel loop intern
+        assertFalse(controlador.esIntentRepetit(Arrays.asList(1, 2, 5, 4)));
+
+        // Cas 4: n pasades (màxim) - mateix intent que intentBase [1,2,3,4]
+        assertTrue(controlador.esIntentRepetit(intentBase));
+
+        /*
+            Loop extern
+        */
+
+        // Cas 1: 0 pasades pel loop extern (historial buit)
+        controlador = new MasterMindControlador(model, vistaMock);
+        assertFalse(controlador.esIntentRepetit(intentBase));
+
+        // Cas 2: 1 pasada pel loop extern (historial.size() == 1)
+        controlador = new MasterMindControlador(model, vistaMock);
+        List<List<Integer>> h1 = new ArrayList<>();
+        h1.add(Arrays.asList(9, 9, 9, 9));
+        inserirHistorial(h1);
+        
+        // Ho provem amb un no repetit
+        assertFalse(controlador.esIntentRepetit(intentBase));
+
+        // Cas 3: 2 pasades pel loop extern (historial.size() == 2)
+        controlador = new MasterMindControlador(model, vistaMock);
+        List<List<Integer>> h2 = new ArrayList<>();
+        h2.add(Arrays.asList(9, 9, 9, 9));
+        h2.add(intentBase);
+        inserirHistorial(h2);
+        
+        assertTrue(controlador.esIntentRepetit(intentBase));
+
+        // Cas 4: m pasades pel loop extern (historial.size() == m (3 en aquest cas))
+        controlador = new MasterMindControlador(model, vistaMock);
+        List<List<Integer>> h3 = new ArrayList<>();
+        h3.add(Arrays.asList(9, 9, 9, 9));
+        h3.add(Arrays.asList(8, 8, 8, 8));
+        h3.add(intentBase);
+        inserirHistorial(h3);
+        
+        assertTrue(controlador.esIntentRepetit(intentBase));
+        
+        // Cas 5: n pasades pel loop extern (historial.size() == n) - No troba res al historial, acaba retornant fals
+        controlador = new MasterMindControlador(model, vistaMock);
+        inserirHistorial(h3);
+        
+        assertFalse(controlador.esIntentRepetit(Arrays.asList(0, 0, 0, 0)));
 
     }
 }
